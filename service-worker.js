a9eec74c865e975cfdd887367c2bf629b66f8bc2
szrexzhu <yx113355@qq.com>
2015-12-14 16:28:05 +0800

    脚本实现

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,81 @@
  * Time: 下午3:11
  * To change this template use File | Settings | File Templates.
  */
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/swscript/main.js'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                debugger;
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+
+
+//self.addEventListener('fetch', function(event) {
+//    event.respondWith(
+//        caches.match(event.request)
+//            .then(function(response) {
+//                debugger;
+//                console.log('缓存命中');
+//                // Cache hit - return response
+//                if (response) {
+//                    console.log(response=' + response);
+//                    return response;
+//                }
+//
+//                // IMPORTANT: Clone the request. A request is a stream and
+//                // can only be consumed once. Since we are consuming this
+//                // once by cache and once by the browser for fetch, we need
+//                // to clone the response
+//                var fetchRequest = event.request.clone();
+//
+//                return fetch(fetchRequest).then(
+//                    function(response) {
+//                        debugger;
+//                        // Check if we received a valid response
+//                        if(!response || response.status !== 200 || response.type !== 'basic') {
+//                            return response;
+//                        }
+//
+//                        // IMPORTANT: Clone the response. A response is a stream
+//                        // and because we want the browser to consume the response
+//                        // as well as the cache consuming the response, we need
+//                        // to clone it so we have 2 stream.
+//                        var responseToCache = response.clone();
+//
+//                        caches.open(CACHE_NAME)
+//                            .then(function(cache) {
+//                                cache.put(event.request, responseToCache);
+//                            });
+//
+//                        return response;
+//                    }
+//                );
+//            })
+//    );
+//});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                debugger;
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request);
+            }
+        )
+    );
+});
---
+++ swscript/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: rexzhu
+ * Date: 15-12-14
+ * Time: 下午4:20
+ * To change this template use File | Settings | File Templates.
+ */
+
+alert("test service worker!");
